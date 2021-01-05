fab9ba638de79e640218a273d05560815a1b4090
tejasvinu <tejasvinu@gmail.com>
2021-01-05 12:34:20 +0530

    .

--- landing_page.php
+++ landing_page.php
@@ -126,7 +126,7 @@ body, html {
     <span class="w3-jumbo w3-hide-small">Rent cars with style</span><br>
     <span class="w3-xxlarge w3-hide-large w3-hide-medium">rent cars with style</span><br>
     <span class="w3-large">Stop wasting valuable time with cars that aren't you</span>
-    <p><a href="browsing1.php" class="w3-button w3-white w3-padding-large w3-large w3-margin-top w3-opacity w3-hover-opacity-off">Start a lifestyle now by clicking here</a></p>
+    <p><a href="browsing.html" class="w3-button w3-white w3-padding-large w3-large w3-margin-top w3-opacity w3-hover-opacity-off">Start a lifestyle now by clicking here</a></p>
   </div>
   <div class="w3-display-bottomleft w3-text-grey w3-large" style="padding:24px 48px">
     <i class="fa fa-facebook-official w3-hover-opacity"></i>
